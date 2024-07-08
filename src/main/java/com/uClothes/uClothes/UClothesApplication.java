a49ce54df476ce5c490053338f9d38795702f0e9
Bryła <csowscy@gmail.com>
2024-07-08 20:17:39 +0200

    Add connection to db

--- src/main/java/com/uClothes/uClothes/UClothesApplication.java
+++ src/main/java/com/uClothes/uClothes/UClothesApplication.java
@@ -1,15 +1,27 @@
 package com.uClothes.uClothes;
 
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 
 @SpringBootApplication
 public class UClothesApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        String dbUsername = dotenv.get("DB_USERNAME");
+        String dbPassword = dotenv.get("DB_PASSWORD");
+        String dbUrl = dotenv.get("DB_URL");
+
+        if (dbUsername == null || dbPassword == null || dbUrl == null) {
+            System.err.println("One or more required environment variables are missing. Please check your .env file.");
+            System.exit(1);
+        }
+
+        System.setProperty("DB_USERNAME", dbUsername);
+        System.setProperty("DB_PASSWORD", dbPassword);
+        System.setProperty("DB_URL", dbUrl);
         SpringApplication.run(UClothesApplication.class, args);
     }
 
