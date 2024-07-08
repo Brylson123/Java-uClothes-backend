0b700ee73908f3661b166367d05b7a8864ca3e31
Bryła <csowscy@gmail.com>
2024-07-08 20:15:25 +0200

    Init commit

---
+++ src/main/java/com/uClothes/uClothes/UClothesApplication.java
@@ -0,0 +1,16 @@
+package com.uClothes.uClothes;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class UClothesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UClothesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uClothes/uClothes/UClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uClothes.uClothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
