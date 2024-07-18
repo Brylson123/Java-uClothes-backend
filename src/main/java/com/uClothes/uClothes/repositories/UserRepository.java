6f988d569342081ee4129d01146b26f74dff448d
Bryła <csowscy@gmail.com>
2024-07-18 23:27:54 +0200

    Add methods

--- src/main/java/com/uClothes/uClothes/repositories/UserRepository.java
+++ src/main/java/com/uClothes/uClothes/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package com.uClothes.uClothes.repositories;
 
 import com.uClothes.uClothes.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+    User findByCurrentTokenId(String currentTokenId);
 }
