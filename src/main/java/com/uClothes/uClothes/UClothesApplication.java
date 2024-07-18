287b32d1ba49f6d6af106fdfbf658ae77cd6fbae
Bryła <csowscy@gmail.com>
2024-07-18 23:37:59 +0200

    Load JwtSecret from .env and add folder uploads for photos

--- src/main/java/com/uClothes/uClothes/UClothesApplication.java
+++ src/main/java/com/uClothes/uClothes/UClothesApplication.java
@@ -4,6 +4,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 
 @SpringBootApplication
 public class UClothesApplication {
@@ -13,15 +15,26 @@ public class UClothesApplication {
         String dbUsername = dotenv.get("DB_USERNAME");
         String dbPassword = dotenv.get("DB_PASSWORD");
         String dbUrl = dotenv.get("DB_URL");
+        String jwtSecret = dotenv.get("JWT_SECRET");
 
-        if (dbUsername == null || dbPassword == null || dbUrl == null) {
+
+        if (dbUsername == null || dbPassword == null || dbUrl == null || jwtSecret == null) {
             System.err.println("One or more required environment variables are missing. Please check your .env file.");
             System.exit(1);
         }
-
+        System.out.println(jwtSecret);
         System.setProperty("DB_USERNAME", dbUsername);
         System.setProperty("DB_PASSWORD", dbPassword);
         System.setProperty("DB_URL", dbUrl);
+        System.setProperty("JWT_SECRET", jwtSecret);
+
+        File uploadDir = new File("uploads");
+        if (!uploadDir.exists()) {
+            if (!uploadDir.mkdirs()) {
+                System.err.println("Failed to create upload directory.");
+                System.exit(1);
+            }
+        }
         SpringApplication.run(UClothesApplication.class, args);
     }
 
