0bde721e16e141654f8cfb3a1359a74429b00ba6
Bryła <csowscy@gmail.com>
2024-07-18 23:33:30 +0200

    Add method

--- src/main/java/com/uClothes/uClothes/repositories/ClothesRepository.java
+++ src/main/java/com/uClothes/uClothes/repositories/ClothesRepository.java
@@ -1,9 +1,12 @@
 package com.uClothes.uClothes.repositories;
 
 import com.uClothes.uClothes.domain.ClothesOffer;
+import com.uClothes.uClothes.domain.ClothingCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ClothesRepository extends JpaRepository<ClothesOffer, UUID> {
+    List<ClothesOffer> findByClothingCategory(ClothingCategory category);
 }
