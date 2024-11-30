cd10593579f2c1681658b4130130ac29eb513685
Bryła <csowscy@gmail.com>
2024-11-30 23:02:07 +0100

    add payment method

--- src/main/java/com/uClothes/uClothes/config/MailConfig.java
+++ src/main/java/com/uClothes/uClothes/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.uClothes.uClothes.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("uclothes254@gmail.com");
-        mailSender.setPassword("zqyk dejl iaiz aito");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
